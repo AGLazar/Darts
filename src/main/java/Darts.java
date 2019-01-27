82a8995c04d6999e196a47fe2de0d20facff3098
=AGLazar <=alexandrugeorge.lazar@gmail.com>
2019-01-27 01:30:20 -0800

    FirstCommit

---
+++ src/main/java/Darts.java
@@ -0,0 +1,13 @@
+public class Darts {
+    enum Multyplier {
+        DOUBLE, TRIPLE
+    }
+
+    int score = 301;
+    int dartsLeft = 3;
+    boolean isFinished = false;
+    int turn = 1;
+    int lastTurnScore = score;
+
+
+}
---
+++ src/test/java/DartsTest.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+
+public class DartsTest {
+    private Darts myDartsGame;
+
+    @Before
+    public void setUp() {
+        myDartsGame = new Darts();
+    }
+
+}
